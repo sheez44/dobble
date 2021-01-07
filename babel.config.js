485714e0915056417418e6c02f976b124fe4a708
Johan <blaat2k@mail.com>
2021-01-08 00:12:11 +0100

    Add vue sass plugin and set it up globally

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/scss/_variables.scss";
+          @import "@/scss/_mixins.scss";
+        `
+      }
+    }
+  }
 }

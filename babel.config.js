5dd00420b69d5d3dfa9a800e561fe55af25f2650
Johan <blaat2k@mail.com>
2021-01-08 16:46:36 +0100

    Update babel config, remove obsolete options

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `
-          @import "@/scss/_variables.scss";
-          @import "@/scss/_mixins.scss";
-        `
-      }
-    }
-  }
+  ]
 }
